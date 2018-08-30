0abb44511347ebfdea7f3e624a86f369b232cdfd
Matteo Gaito <matteo.gaito@gmail.com>
2018-08-31 00:17:18 +0200

    refactoring grafico

--- app/home.py
+++ app/home.py
@@ -5,7 +5,11 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route("/aste")
+def aste():
+    return render_template('aste.html')
 
 @app.route("/error")
 def error():
