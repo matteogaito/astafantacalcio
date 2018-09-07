3e9e711e9946bca0ff5c9a3a84ae50e59a1eb82e
Matteo Gaito <matteo.gaito@gmail.com>
2018-09-07 14:20:22 +0200

    added contact form

--- app/home.py
+++ app/home.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template
+from flask import render_template, request
 
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import TextField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+from wtforms.validators import Required
 
 @app.route("/")
 def index():
@@ -14,3 +19,23 @@ def aste():
 @app.route("/error")
 def error():
     return render_template('error.html')
+
+class ContactForm(FlaskForm):
+    name = TextField("Name")
+    email = TextField("Email")
+    subject = TextField("Subject")
+    message = TextAreaField("Message")
+    recaptcha = RecaptchaField()
+    submit = SubmitField("Send")
+
+@app.route('/contatti', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    print(app.testing)
+
+    if request.method == 'POST':
+        return 'Form posted.'
+
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
